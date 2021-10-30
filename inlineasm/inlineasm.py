35b700d3d5abcd8d582c72131f9ff676b723de08
i0x <43708417+ay0ks@users.noreply.github.com>
2021-10-30 18:14:23 +0200

    Update inlineasm.py

--- inlineasm/inlineasm.py
+++ inlineasm/inlineasm.py
@@ -2,19 +2,62 @@
 # feel free to modify or copy this code 
 # but also specify credits (link to this repo or file)
 
-def __e(obj,n,v):obj[n]=v
-def __raise(e):raise e
-___e,__,___,__c,__p,__if,___join=(lambda obj,n,v:setattr(obj,n,v)),(lambda ret,*argc:ret),(lambda *argc:0),(lambda obj,n:___e(globals(),obj,{**obj,n:""})),(lambda obj:print(obj)),(lambda*__:__[0]if __[1]else __[2]),(lambda a:[a[0],a[1]])
-__import=type("__import",(),{"__init__":lambda self:None,"__lshift__":lambda self,mod:__(self,___e(self,"mod",mod),__e(globals(),mod,__import__(mod))),"__lt__":lambda self,sub:__(self,__e(globals(),sub,getattr(__import__(getattr(self,"mod")),sub)))if type(sub)==str else[__(self,__e(globals(),sub_,getattr(__import__(self.mod),sub_)))for sub_ in sub]})()
-__include=type("__include",(),{"__init__":lambda self:None,"__lt__":lambda self,file:exec(open(file,"r").read())})()
+def ___dictappend(object_,name,value):
+ object_[name]=value
+def __delete(object_):
+ del object_
+def __raise(e):
+ raise exception
+___rmulti=lambda to_return,*argc:to_return
+___multi=lambda*argc:0
+___assign=lambda object,name,value:setattr(object,name,value)
+___acopy=lambda object,name:___assign(globals(),object,{**object,name:""})
+___print=lambda object:print(object)
+___if=lambda do_if,condition,do_else:do_if if condition else do_else
+___join=lambda to_join:[to_join[0],to_join[1]]
+___aiter=lambda action=print,args=(),element="i",to_iter=globals():___rmulti([action(element,*args)for element in to_iter],___multi())
+___iter=lambda element="i",to_iter=globals():___multi([element for element in to_iter])
+___riter=lambda element="i",to_iter=globals():___rmulti([element for element in to_iter],___multi())
+___import=type("__import",(),{"__init__":(lambda self:None),"__lshift__":(lambda self,module:___rmulti(self,___assign(self,"module",module),___dictappend(globals(),module,__import__(module)),)),"__lt__":(lambda self,submodule:___rmulti(self,___dictappend(globals(),submodule,getattr(__import__(getattr(self,"module")),submodule),)if type(submodule)==str else[___rmulti(self,___dictappend(globals(),submodule_,getattr(__import__(self.module),submodule_),),)for submodule_ in submodule],)),"__eq__":(lambda self,alternate:___multi(___dictappend(globals(),alternate,globals()["submodule"]),__del(globals()["submodule"]),)),},)()
+___include=type("__include",(),{"__init__":(lambda self:None),"__lt__":(lambda self,file:exec(open(file,"r").read())),})()
 
-__af=type("__af",(object,),{"__init__":(lambda self,asm:__(None,___(___(___e(self,"tfile",___join(tempfile.mkstemp(".S","temp",os.getcwd()))),___e(self,"source",getattr(self,"tfile")[1]),___e(self,"f",open(getattr(self,"source"),"wb")),),___(getattr(self,"f").write(asm.encode("utf-8")),getattr(self,"f").close(),),___(___e(self,"sts",subprocess.check_output(f"nasm {getattr(self,'source')}",shell=True)),__if(print(subprocess.check_output(getattr(self,"source")[:-2]).decode("utf-8")),getattr(self,"sts")==0,print(getattr(self,"sts"))),),os.unlink(getattr(self,"source")))))})
-__asm=type("__asm",(),{"__init__":lambda self:None,"__lt__":(lambda self,asm:__af(asm if type(asm)is str else'\n'.join(asm)))})()
+___assembly=type("___assembly",(object,),{
+ "__init__":(lambda self,asm:
+  ___rmulti(None,
+  ___multi(
+  ___multi(
+  ___assign(self, "temp", ___join(tempfile.mkstemp(".S","temp", os.getcwd()))),
+  ___assign(self, "source", getattr(self, "temp")[1]),
+  ___assign(self, "file", __s := open(getattr(self, "source"), "wb")),
+  ),
+  ___multi(
+  getattr(self, "file").write(asm.encode("utf-8")),
+  getattr(self, "file").close(),
+  ),
+  ___multi(
+  ___assign(self, "status", subprocess.check_output(f"nasm {getattr(self, 'source')}", shell=True)),
+  ___if(
+   ___print(subprocess.check_output(getattr(self, "source")[:-2]).decode("utf-8")),
+   getattr(self, "status") == 0,
+   ___print(getattr(self, "status"))
+  ),
+  ),
+  __s.close() and os.unlink(getattr(self, "source"))
+  )
+  )
+ )
+})
+__asm=type("__asm",(),{
+ "__init__":(lambda self:None),
+ "__lt__":(lambda self,asm:
+  ___assembly(asm if type(asm) is str else '\n'.join(asm))
+ )
+})()
 
 
-__import <<"os"
-__import <<"tempfile"
-__import <<"subprocess"
+___import <<"os"
+___import <<"tempfile"
+___import <<"subprocess"
 
 __asm <("""
 
